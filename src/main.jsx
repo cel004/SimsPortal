4d93ca1575eb1aa158e866eb2d74585d20ad2660
cel004 <ngcelinaa@gmail.com>
2025-03-20 19:44:16 -0400

    Update nav bar and logo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Syne:wght@400..800&display=swap');
+    </style>
+    <div className="heroContainer">
+      <div className="barContainer">
+        <div className="titleContainer">
+          <img src="./LOGONAME.svg"></img>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      <h1 className = "title">Search all Sims mods in one place.</h1>
+    </div>
+    
   </StrictMode>,
 )
