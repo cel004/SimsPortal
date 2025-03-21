9db0df642947b7e6c7467999452c66ada5fde982
cel004 <ngcelinaa@gmail.com>
2025-03-20 22:27:04 -0400

    Create features section
    TBA: scroll images

--- src/main.jsx
+++ src/main.jsx
@@ -22,14 +22,16 @@ createRoot(document.getElementById('root')).render(
           </ul>
         </nav>
       </div>
-      <h1 className = "title">Search all Sims mods in one place.</h1>
+      <h1 className = "title" style={{paddingTop: "10rem"}}>Search all Sims mods in one place.</h1>
       <p className = "subtitle" style={{ marginTop: '-0.5rem' }}>The easiest way to browse, sort, and download Sims mods.</p>
       <div className="searchContainer">
         <input type="text" placeholder="Search for mods by keywords"></input>
         <img id="glass" src="./search_button.png"></img>
       </div>
-      <p className="text" style={{ fontWeight: '300', padding: '0 1rem 0 1rem'}}>(e.g., 'Maxis Match', 'Alpha'), game version (e.g., 'Sims 4'), or mod type (e.g., 'CAS', 'UI').</p>
-    
+      <p className="text" style={{ fontWeight: '300', padding: '0 1rem 7rem 1rem'}}>Keywords (e.g., 'Maxis Match'), game version (e.g., 'Sims 4'), or mod type (e.g., 'CAS', 'UI').</p>
+      <div className ="featureContainer">
+        <h1 className ="title" style={{paddingTop: "2rem", paddingBottom: "5rem"}}>Featured Mods</h1>
+      </div>
     </div>
     
   </StrictMode>,
