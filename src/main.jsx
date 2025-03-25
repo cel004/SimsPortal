34dd3eb892e49e80bdce08c25b3d38bc5470e3bc
cel004 <ngcelinaa@gmail.com>
2025-03-25 19:39:04 -0400

    Add page routing and pages folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,7 +12,11 @@ createRoot(document.getElementById('root')).render(
       @import url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Syne:wght@400..800&display=swap');
     </style>
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
